1c7d56a4a452198d324d6af4b86336ea66f97259
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-09-24 20:34:06 +0800

    chore: make linter ignore prisma client

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import perfectionist from 'eslint-plugin-perfectionist';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default defineConfig(
+  [globalIgnores(['**/generated/'])],
   {
     ignores: ['**/*.js'],
   },
