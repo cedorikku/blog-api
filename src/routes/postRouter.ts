5bb424cf0bee1030c2463be81d0977a193d80dd3
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-10-17 14:51:42 +0800

    feat: create post creation schema validation

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import postController from '../controllers/postController.js';
+import { validate } from '../middlewares/validation.js';
+import { createPostSchema } from '../schemas/postSchema.js';
 
 const router = Router();
 
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getPostById);
-router.post('/', postController.createPost);
+router.post('/', validate(createPostSchema), postController.createPost);
 router.delete('/:id', postController.deletePost);
 // router.patch('/:id', postController.editPost)
 
---
+++ src/schemas/postSchema.ts
@@ -0,0 +1,22 @@
+import * as z from 'zod';
+
+export const createPostSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(1, 'Title cannot be empty')
+    .max(50, 'Title can only be 50 characters long'),
+  content: z.string().trim().min(1, 'Post content cannot be empty'),
+  tags: z
+    .array(z.string().trim())
+    .min(1, 'Should have at least 1 tag value')
+    .superRefine((val, ctx) => {
+      if (val.length !== new Set(val).size) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'No duplicates allowed',
+          input: val,
+        });
+      }
+    }),
+});
