14ebee726724d38aae5b903b0a1a62ccdf1e8101
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-10-21 21:52:43 +0800

    feat: insert middleware for get and delete post routes
    Specifically, auth, loader and perms middlewares

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,16 +1,37 @@
 import { Router } from 'express';
 
 import postController from '../controllers/postController.js';
-import auth from '../middlewares/auth.js';
+import auth, { authAllowGuest } from '../middlewares/auth.js';
+import { loadPost } from '../middlewares/loader.js';
+import {
+  PostAction,
+  permitAction,
+} from '../middlewares/postPermissions.js';
 import { validate } from '../middlewares/validation.js';
 import { createPostSchema } from '../schemas/postSchema.js';
 
 const router = Router();
 
 router.get('/', postController.getAllPosts);
-router.get('/:id', postController.getPostById);
+
+router.get(
+  '/:id',
+  authAllowGuest,
+  loadPost,
+  permitAction(PostAction.View),
+  postController.getPostById
+);
+
 router.post('/', auth, validate(createPostSchema), postController.createPost);
-router.delete('/:id', postController.deletePost);
+
+router.delete(
+  '/:id',
+  auth,
+  loadPost,
+  permitAction(PostAction.Delete),
+  postController.deletePost
+);
+
 // router.patch('/:id', postController.editPost)
 
 export default router;
