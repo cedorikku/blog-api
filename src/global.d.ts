a8e90fba72b70d77675c090b24495d50463c3e46
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-10-21 20:57:27 +0800

    feat(ts): attach 'post' type in Requests

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,11 @@
 import type { User as PrismaUser } from '@prisma/client';
+import type { Post } from '@prisma/client';
 
 declare global {
   namespace Express {
+    interface Request {
+      post?: Post;
+    }
     // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface User extends PrismaUser {}
   }
