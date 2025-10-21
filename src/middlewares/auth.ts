133038c2b9a715c7da7b99f5d75e7f2cb91558bf
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-10-21 21:02:35 +0800

    feat: create auth (allow guest) middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,3 +1,6 @@
+import type { NextFunction, Request, Response } from 'express';
+import type { AuthenticateCallback } from 'passport';
+
 import passport from '../config/passport.js';
 
 const auth = passport.authenticate('jwt', {
@@ -6,3 +9,20 @@ const auth = passport.authenticate('jwt', {
 });
 
 export default auth;
+
+export const authAllowGuest = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const cb: AuthenticateCallback = (err, user) => {
+    if (err) {
+      return next(err);
+    }
+
+    req.user = user ? user : undefined;
+    next();
+  };
+
+  return passport.authenticate('jwt', { session: false }, cb)(req, res, next);
+};
