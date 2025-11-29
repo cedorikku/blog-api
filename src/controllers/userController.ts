9e76b2897750af063736bbbcef47c7f3ce7db60d
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-11-29 17:43:17 +0800

    feat: add get user posts endpoint

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -12,6 +12,74 @@ const checkUsernameAvailability = async (req: Request, res: Response) => {
   res.status(200).json({ availability: !user });
 };
 
+const getPostsByUsername = async (req: Request, res: Response) => {
+  const username = req.params.username;
+
+  const user = await prisma.user.findUnique({
+    where: { username: username.trim().toLowerCase() },
+  });
+
+  if (!user) {
+    res.sendStatus(404);
+  }
+
+  // Based on who's requesting
+  const requestingUser = req.user ?? null;
+
+  let posts = [];
+  if (requestingUser && requestingUser.username === username) {
+    posts = await prisma.post.findMany({
+      select: {
+        id: true,
+        title: true,
+        createdAt: true,
+        modifiedAt: true,
+        slug: true,
+        author: {
+          select: { username: true, name: true },
+        },
+        tags: {
+          select: {
+            tag: {
+              select: { id: true, name: true },
+            },
+          },
+        },
+      },
+      where: {
+        author: { username },
+      },
+    });
+  } else {
+    posts = await prisma.post.findMany({
+      select: {
+        id: true,
+        title: true,
+        createdAt: true,
+        modifiedAt: true,
+        slug: true,
+        author: {
+          select: { username: true, name: true },
+        },
+        tags: {
+          select: {
+            tag: {
+              select: { id: true, name: true },
+            },
+          },
+        },
+      },
+      where: {
+        author: { username },
+        published: true,
+      },
+    });
+  }
+
+  res.status(200).json(posts);
+};
+
 export default {
   checkUsernameAvailability,
+  getPostsByUsername,
 };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import userController from '../controllers/userController.js';
+import { authAllowGuest } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.get('/:username/check', userController.checkUsernameAvailability);
+router.get('/:username/posts', authAllowGuest, userController.getPostsByUsername);
 
 export default router;
